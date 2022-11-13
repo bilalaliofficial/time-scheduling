a93e792ccd41f1272c12a9f2d5bd00f14969d3ba
Bilal Ali <bilalali0002@gmail.com>
2022-11-13 20:08:49 +0500

    Skip break during slot creation

--- database/seeders/SlotSeeder.php
+++ database/seeders/SlotSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Breaks;
 use App\Models\Schedule;
 use App\Models\Slot;
 use Carbon\Carbon;
@@ -21,6 +22,7 @@ class SlotSeeder extends Seeder
             $slots = [];
             $start_time = null;
             foreach ($schedules as $schedule){
+                $breaks = Breaks::where('schedule_id',$schedule->schedule_id)->get(['start_time','end_time']);
                 $start_time = Carbon::parse($schedule->opening_time)->format('H:i');
                 $end_time = Carbon::parse($schedule->closing_time)->format('H:i');
                 while (strtotime($start_time) <= strtotime($end_time)){
@@ -28,6 +30,15 @@ class SlotSeeder extends Seeder
                     $end = Carbon::parse($start_time)->addMinutes($schedule->slot_duration)->format('H:i');
                     $start_time = Carbon::parse($start_time)->addMinutes($schedule->slot_duration+$schedule->buffer_time)->format('H:i');
                     if (strtotime($start_time) <= strtotime($end_time)){
+                        if (!empty($breaks)){
+                            foreach ($breaks as $break){
+                                if($this->time_range($break->start_time,$break->end_time,$start_time)){
+                                    $start = $break->end_time;
+                                    $end = Carbon::parse($break->end_time)->addMinutes($schedule->slot_duration)->format('H:i');
+                                    $start_time = Carbon::parse($break->end_time)->addMinutes($schedule->slot_duration)->format('H:i');
+                                }
+                            }
+                        }
                         $slots[] = [
                             'schedule_id'   => $schedule->schedule_id,
                             'start_time'    => $start,
@@ -43,5 +54,17 @@ class SlotSeeder extends Seeder
                 Slot::insert($slots);
             }
         }
+    }
+
+    public function time_range($start, $end,$time)
+    {
+        if($start > $end) {
+            if($time >= $start || $time < $end){
+                return true;
+            }
+        } else if ($time >= $start && $time <= $end) {
+            return true;
+        }
+        return false;
     }
 }
